52b099e28530735f9bcd38f795d669fc8f93dc09
lucky624 <46750139+lucky624@users.noreply.github.com>
2019-04-06 18:53:15 +0600

    Add files via upload

---
+++ server/config.py
@@ -0,0 +1,42 @@
+CONFIG = {
+
+    'TEAMS': {
+        'void*':'10.0.0.1',
+        'HawkSquad':'10.0.0.2',
+        '0xIDEA':'10.0.0.3',
+        'rwx':'10.0.0.4',
+    },
+
+
+    'FLAG_FORMAT': r'[A-Z0-9]{31}=',
+
+
+
+    'SYSTEM_PROTOCOL': 'ructf_tcp',
+    'SYSTEM_HOST': '127.0.0.1',
+    'SYSTEM_PORT': 31337,
+
+
+    'SUBMIT_FLAG_LIMIT': 50,
+    'SUBMIT_PERIOD': 5,
+    'FLAG_LIFETIME': 5,
+
+
+    'SERVER_IP': '0.0.0.0',
+    'SERVER_PASSWORD': '1234',
+
+
+
+
+    'ENABLE_API_AUTH': False,
+    'API_TOKEN': '00000000000000000000',
+
+
+    #START_TIME - print(datetime.now())
+    'GRAPHICS_REFRESH_INTERVAL': 10,        #Seconds
+    'START_TIME':'2019-04-06 16:04:00',
+    'ROUND_PERIOD': 60                      #Seconds
+
+
+
+}
